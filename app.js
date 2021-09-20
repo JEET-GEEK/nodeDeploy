ea78cf837a0dc1c2e5e0e7a1025b320fcf1c9caa
Lakshmi Roy <lakshmi.roy@appzlogic.com>
2021-09-20 12:08:46 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = process.env.port || 4000;
+
+app.get("/", (req,res) => {
+    res.send("Hello world!!!");
+})
+
+app.listen(port , () => {
+    console.log(`server is listening on port ${port}`)
+})
